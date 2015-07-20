f0f23e15ca00c452c25007432dc713b0933c2928
longleggedone <longlegsclassic@gmail.com>
2015-07-20 13:37:19 -0400

    RubeGoldberg homework prototype

---
+++ Assets/scripts/triggerdemo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerdemo : MonoBehaviour {
+
+	//space to set desired particle effect
+	public GameObject particleEffect;
+	bool didITriggerYet = false;
+	// Update is called once per frame
+	void Update () {
+		if (didITriggerYet == true){
+			//turns on inactive particle effect, make sure it is deactive beforehand
+			particleEffect.SetActive(true);
+			//Debug.Log ("it worked");
+		}
+	}
+	//whenever an object with a rigidbody and a collider enters the trigger the code will execute
+	void OnTriggerEnter(){
+		didITriggerYet = true;
+	}
+}
